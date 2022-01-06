b4cb08202c9b33d2cd0b8b35d79d1893588a73d8
felixmerz <felixmerz00rgmail.com>
2022-01-06 18:02:50 +0100

    User Input hinzugefügt. Das Spiel ist nun spielbar.

--- Project14_Tictactoe/main.c
+++ Project14_Tictactoe/main.c
@@ -2,15 +2,43 @@
 #include <stdlib.h>
 
 void draw_field(char positions[3][3]);
+int check_for_win(char positions[3][3]);
 
 int main()
 {
-    char positions[3][3] = {{'o', 'o', 'x'}, {'x', 'o', 'o'}, {'x', 'x', 'o'}};
+    int gewinner_existiert = 0;
+    char positions[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
     draw_field(positions);
-    check_for_win(positions);
+    gewinner_existiert = check_for_win(positions);
+
+    int x_pos;
+    int y_pos;
+    char ch;
+    while(gewinner_existiert == 0){
+        /*Neuer input vom Spieler einlesen.*/
+        printf("In welcher Zeile (1/2/3) möchtest du ein Zeichen setzen? ");
+        scanf("%d", &x_pos);
+        x_pos--;
+        printf("In welcher Spalte (1/2/3) möchtest du ein Zeichen setzen? ");
+        scanf("%d", &y_pos);
+        y_pos--;
+        printf("Welches Zeichen mchtest du setzen (x/o)? ");
+        scanf(" %c", &ch);
+        if(x_pos < 0 || x_pos > 2 || y_pos < 0 || y_pos > 2 || (ch != 'x' && ch != 'o')){
+            printf("Ein eingegeben Wert is ungültig.\n");
+            continue;
+        }
+
+        /*Input vom Spieler auf dem Spielfeld einzeichnen und überprüfen ob er gewonnen hat.*/
+        positions[x_pos][y_pos] = ch;
+        draw_field(positions);
+        gewinner_existiert = check_for_win(positions);
+    }
+
     return 0;
 }
 
+/*Spielfeld in Konsole malen*/
 void draw_field(char positions[3][3])
 {
     printf("\n%c|%c|%c\n", positions[0][0], positions[0][1], positions[0][2]);
@@ -20,6 +48,7 @@ void draw_field(char positions[3][3])
     printf("%c|%c|%c\n\n", positions[2][0], positions[2][1], positions[2][2]);
 }
 
+/*Überprüfen ob ein spieler gewonnen hat. Returned 1 falls es einen Gewinner gibt, 0 falls es keinen Gewinner gibt.*/
 int check_for_win(char positions[3][3])
 {
     /*Zeilen überprüfen*/
@@ -33,6 +62,7 @@ int check_for_win(char positions[3][3])
                 }
                 if(score == 3){
                     printf("%c wins the game!\n", current_char);
+                    return 1;
                 }
             }
         }
@@ -49,17 +79,20 @@ int check_for_win(char positions[3][3])
                 }
                 if(score == 3){
                     printf("%c wins the game!\n", current_char);
+                    return 1;
                 }
             }
         }
     }
     /*Diagonale 1 überprüfen*/
-    if(positions[0][0] == positions[1][1] && positions[0][0] == positions[2][2]){
+    if(positions[0][0] != ' ' && positions[0][0] == positions[1][1] && positions[0][0] == positions[2][2]){
         printf("%c wins the game!\n", positions[0][0]);
+        return 1;
     }
     /*Diagonale 2 überprüfen*/
-    if(positions[0][2] == positions[1][1] && positions[0][2] == positions[2][0]){
+    if(positions[0][2] != ' ' && positions[0][2] == positions[1][1] && positions[0][2] == positions[2][0]){
         printf("%c wins the game!\n", positions[0][2]);
+        return 1;
     }
-
+    return 0;
 }
